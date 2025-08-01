b715addcb844af5ad6e48440559f5c05d7ce55db
Roxanne-Mary Bell <96121350+roxanne007@users.noreply.github.com>
2025-08-01 18:13:08 +1000

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -7,21 +7,21 @@ btnHamburger.addEventListener('click', function () {
   body.classList.toggle('noscroll');
 });
 
-// Articles
 const allArticles = [
-  { img: '/images/heels.jpg', title: 'The ONLY Heels You Need This Summer' },
-  { img: '/images/loungewear.jpg', title: 'Stylish new loungewear for lockdown' },
-  { img: '/images/metgala.jpg', title: 'Everything we know about the 2021 Met Gala' },
-  { img: '/images/summertrends.png', title: 'The Top Trends for Summer!' },
-  { img: '/images/tennis.jpg', title: "Kith x Wilson's Tennis Collection Is Preppy-Cool" },
-  { img: '/images/summerfashion.jpg', title: 'Your Summer Wardrobe Is Here' },
-  { img: '/images/redcarpet.jpg', title: 'Red carpet looks for the 2022 Film Festival' },
-  { img: '/images/shoehistory.jpg', title: 'Iconic Shoe Moments in Movie History' },
-  { img: '/images/redhair.jpg', title: '28 Halloween Costumes for People With Red Hair' },
-  { img: '/images/halloween-costumes.jpg', title: '10 Halloween Costumes for a College Party' },
-  { img: '/images/falltrends.png', title: 'These 10 Trends Will Really Take off Next Fall' },
-  { img: '/images/amazonsale.jpg', title: 'Amazon Is Having a Secret Summer Sale' }
+  { img: 'images/heels.jpg', title: 'The ONLY Heels You Need This Summer' },
+  { img: 'images/loungewear.jpg', title: 'Stylish new loungewear for lockdown' },
+  { img: 'images/metgala.jpg', title: 'Everything we know about the 2021 Met Gala' },
+  { img: 'images/summertrends.png', title: 'The Top Trends for Summer!' },
+  { img: 'images/tennis.jpg', title: "Kith x Wilson's Tennis Collection Is Preppy-Cool" },
+  { img: 'images/summerfashion.jpg', title: 'Your Summer Wardrobe Is Here' },
+  { img: 'images/redcarpet.jpg', title: 'Red carpet looks for the 2022 Film Festival' },
+  { img: 'images/shoehistory.jpg', title: 'Iconic Shoe Moments in Movie History' },
+  { img: 'images/redhair.jpg', title: '28 Halloween Costumes for People With Red Hair' },
+  { img: 'images/halloween-costumes.jpg', title: '10 Halloween Costumes for a College Party' },
+  { img: 'images/falltrends.png', title: 'These 10 Trends Will Really Take off Next Fall' },
+  { img: 'images/amazonsale.jpg', title: 'Amazon Is Having a Secret Summer Sale' }
 ];
+
 
 let showingAll = false;
 const grid = document.getElementById('articleGrid');
